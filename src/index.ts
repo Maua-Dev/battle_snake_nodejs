5e4eb4142b457c9e37562e7db000fac3eba9a469
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-01 15:46:01 -0300

    added full logic of repo in express

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({
+        apiversion: "1",
+        author: "Maua-Dev",
+        color: "#8B0000",
+        head: "tiger-king",
+        tail: "hook",
+        version: "1.0.0"
+    });
+});
+
+app.post('/start', (req: Request, res: Response) => {
+    res.send("ok");
+});
+
+app.post('/move', (req: Request, res: Response) => {
+    console.log(req.body);
+    const directions = ["up", "down", "left", "right"];
+    const i = Math.floor(Math.random() * directions.length);
+    const response = {
+        move: directions[i],
+        shout: `I'm moving ${directions[i]}!`
+    };
+    res.json(response);
+});
+
+app.post('/end', (req: Request, res: Response) => {
+    res.send("ok");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
