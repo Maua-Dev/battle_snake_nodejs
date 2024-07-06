cd7d15f675c848031c944dce85c3f98282e26398
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-06 17:32:34 -0300

    refactor: changed the order of creation of each resource on iac stack

--- iac/iac/iac_stack.ts
+++ iac/iac/iac_stack.ts
@@ -8,76 +8,91 @@ import {
   Duration,
   CfnOutput,
   SecretValue,
-  RemovalPolicy
 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { LambdaStack } from './lambda_stack';
+import { envs as environments } from '../envs';
+import { ComparisonOperator } from 'aws-cdk-lib/aws-cloudwatch';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
 
 export class IacStack extends Stack {
   constructor(scope: Construct, constructId: string, props?: StackProps) {
-      super(scope, constructId, props);
-      const githubRef = process.env.GITHUB_REF || '';
+    super(scope, constructId, props);
+    const githubRef = process.env.GITHUB_REF || '';
 
-      let stage;
-      if (githubRef.includes('prod')) {
-          stage = 'PROD';
-      } else if (githubRef.includes('homolog')) {
-          stage = 'HOMOLOG';
-      } else if (githubRef.includes('dev')) {
-          stage = 'DEV';
-      } else {
-          stage = 'TEST';
-      }
+    let stage;
+    if (githubRef.includes('prod')) {
+      stage = 'PROD';
+    } else if (githubRef.includes('homolog')) {
+      stage = 'HOMOLOG';
+    } else if (githubRef.includes('dev')) {
+      stage = 'DEV';
+    } else {
+      stage = 'TEST';
+    }
 
-      const envs = {
-          'STAGE': stage
-      };
+    const envs = {
+      'STAGE': stage
+    };
 
-      const lambdaStack = new LambdaStack(this, envs);
+    const lambdaStack = new LambdaStack(this, envs);
 
-      // user IAM com senha
-      const userName = `${stage}User`;
-      const password = `${stage}UserPassword7@`;
-      const user = new iam.User(this, userName, {
-          userName: userName,
-          password: SecretValue.unsafePlainText(password),
-          passwordResetRequired: true
-      });
+    const userName = `${environments.PROJECT_NAME}User`;
+    const password = `${environments.PROJECT_NAME}UserPassword7@`;
+    const user = new iam.User(this, userName, {
+      userName: userName,
+      password: SecretValue.unsafePlainText(password),
+      passwordResetRequired: true
+    });
 
-      // políticas do IAM para ver os logs chefia
-      user.addToPolicy(new iam.PolicyStatement({
-          actions: ['lambda:*', 'logs:*'],
-          resources: ['*']
-      }));
+    const policy = new iam.Policy(this, `Policy-BattlesnakeNodejs-${stage}`, {
+      statements: [
+        new iam.PolicyStatement({
+          actions: ['lambda:*'],
+          resources: [lambdaStack.lambdaFunction.functionArn]
+        })
+      ]
+    })
 
-      user.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('IAMUserChangePassword'));
+    policy.addStatements(
+      new iam.PolicyStatement({
+        actions: ['logs:*'],
+        resources: ['arn:aws:logs:*:*:*']
+      })
+    )
 
-      // Output do IAM chefe
-      new CfnOutput(this, 'IAMUserOutput', {
-          value: user.userName,
-          exportName: `${stage}UserName`
-      });
+    policy.attachToUser(user)
 
-      new CfnOutput(this, 'IAMUserPasswordOutput', {
-          value: password,
-          exportName: `${stage}UserPassword`
-      });
+    user.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('IAMUserChangePassword'));
+    
+    const alarm = lambdaStack.lambdaFunction.metricInvocations({
+      period: Duration.hours(6)
+    }).createAlarm(this, `${environments.PROJECT_NAME}LambdaAlarm`, {
+      threshold: 5000,
+      evaluationPeriods: 1,
+      comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
+    })
+    
+    const topic = Topic.fromTopicArn(this, `${environments.PROJECT_NAME}Topic`, 
+      `arn:aws:sns:${environments.AWS_REGION}:${environments.AWS_ACCOUNT_ID}:sns-battlesnake`
+    )
+    const snsAction = new SnsAction(topic)
+    alarm.addAlarmAction(snsAction)
+    
+    new CfnOutput(this, 'IAMUserOutput', {
+      value: user.userName,
+      exportName: `${environments.PROJECT_NAME}UserValue`
+    });
+  
+    new CfnOutput(this, 'IAMUserPasswordOutput-FirstTimePWD', {
+      value: password,
+      exportName: `${environments.PROJECT_NAME}IAMUserPasswordOutput-FirstTimePWD`
+    });
 
-      // alarme aquiiii
-      const alarm = new cloudwatch.Alarm(this, 'LambdaAlarm', {
-          metric: lambdaStack.lambdaFunction.metricInvocations({ period: Duration.hours(6) }),
-          threshold: 5000,
-          evaluationPeriods: 1,
-          comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
-      });
-
-      const topic = new sns.Topic(this, 'AlarmTopic', {
-          displayName: 'Alarm Notification Topic'
-      });
-
-      alarm.addAlarmAction(new actions.SnsAction(topic));
-
-      // Adicionar auto destroy
-      // this.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    new CfnOutput(this, 'LambdaConsole', {
+      value: `https://${environments.AWS_REGION}.console.aws.amazon.com/lambda/home?region=${environments.AWS_REGION}#/functions/${lambdaStack.lambdaFunction.functionName}?tab=code`,
+      exportName: `${environments.PROJECT_NAME}LambdaConsoleValue`
+    })
   }
 }
